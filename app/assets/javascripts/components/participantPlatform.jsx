3a891cfc537f071359ae7a9a70124d76a108dc42
Pablo Ledesma <pablo.ledesma.p@gmail.com>
2017-06-28 17:20:50 -0400

    add tab buttons

--- app/assets/javascripts/components/participantPlatform.jsx
+++ app/assets/javascripts/components/participantPlatform.jsx
@@ -1,5 +1,31 @@
 var ParticipantPlatform = React.createClass({
+
+  propTypes: {
+    participants: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
+  },
+
   render: function() {
-    return <h1>Participant Platform</h1>
+    var isTabActivated = true;
+
+    return (
+      //Head Component
+      <div id='participant-platform'>
+        <div id='sub-header'>
+            <div id='nav-tabs-container'>
+              <ul className="nav nav-pills">
+                <li role="presentation" className={isTabActivated ? '' : 'active'}>
+                  <a href="#" className='tab-link'>Participants</a>
+                </li>
+                <li role="presentation" className={isTabActivated ? '' : 'active'}>
+                  <a href="#" className='tab-link'>Add Participant</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        // Table component
+       //  Form Component
+    );
   }
 });
